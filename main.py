da129c4aa503cb17a179d886f524dcacac8dcd78
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2023-10-11 15:29:50 +0900

    로그인 유지 기능 구현

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI,UploadFile,Form,Response
+from fastapi import FastAPI,UploadFile,Form,Response,Depends
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 from fastapi.encoders import jsonable_encoder
 import sqlite3
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 
 con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
@@ -22,13 +24,61 @@ cur.execute(f"""
 
 app = FastAPI()
 
+SECRET = "super-coding"
+manager = LoginManager(SECRET, 'login')
+
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'''name="{data["name"]}"'''
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    user = cur.execute(f"""
+                       SELECT * FROM users WHERE {WHERE_STATEMENTS}
+                       """).fetchone()
+    return user
+
+@app.post('/login')
+def login(id:Annotated[str,Form()], 
+           password:Annotated[str,Form()]):
+    user = query_user(id)
+    if not user:
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+    
+    access_token = manager.create_access_token(data={
+        'sub': {
+        'id':user['id'],
+        'name':user['name'],
+        'email':user['email']
+        }
+    })
+    
+    return {'access_token':access_token}
+    
+
+@app.post('/signup')
+def signup(id:Annotated[str,Form()], 
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+    cur.execute(f"""
+                INSERT INTO users(id, name, email, password)
+                VALUES ('{id}', '{name}', '{email}', '{password}')
+                """)
+    con.commit()
+    return '200'
+
 @app.post('/items')
 async def create_item(image:UploadFile
                 , title:Annotated[str,Form()], 
                 price:Annotated[int,Form()], 
                 discription:Annotated[str,Form()], 
                 place:Annotated[str,Form()],
-                insertAt:Annotated[int,Form()]
+                insertAt:Annotated[int,Form()], 
+                user=Depends(manager)
                 ):
     
     
@@ -42,7 +92,7 @@ async def create_item(image:UploadFile
     return '200'
 
 @app.get('/items')
-async def get_items():
+async def get_items(user=Depends(manager)):
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     rows = cur.execute(f"""
@@ -57,18 +107,6 @@ async def get_image(item_id):
                               SELECT image from items WHERE id={item_id}
                               """).fetchone()[0]
     return Response(content=bytes.fromhex(image_bytes), media_type='/image/*')
-
-@app.post('/signup')
-def signup(id:Annotated[str,Form()], 
-           password:Annotated[str,Form()],
-           name:Annotated[str,Form()],
-           email:Annotated[str,Form()]):
-    cur.execute(f"""
-                INSERT INTO users(id, name, email, password)
-                VALUES ('{id}', '{name}', '{email}', '{password}')
-                """)
-    con.commit()
-    return '200'
     
 
  
